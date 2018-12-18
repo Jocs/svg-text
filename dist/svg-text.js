7e05c17e24cca969d28a4c7ae6be31e9769b2a40
jocs <ransixi@gmail.com>
2018-12-18 23:43:21 +0800

    publish 0.5.3

--- dist/svg-text.js
+++ dist/svg-text.js
@@ -1,4 +1,4 @@
-/*! svg-text2 v0.5.2 */
+/*! svg-text2 v0.5.3 */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
